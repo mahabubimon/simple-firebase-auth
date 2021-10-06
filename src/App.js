c4f6743bf3fe74aed7aa6d436b0f2bff37a3d745
mahabubimon <mahabubemon24@gmail.com>
2021-10-07 00:55:28 +0600

    signout done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, GithubAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { useState } from "react";
 import "./App.css";
 import initializeAuthentication from "./Firebase/firebase.initialize";
 
 initializeAuthentication();
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+
+const gitHubProvider = new GithubAuthProvider();
 
 function App() {
   const [user, setUser] = useState({});
   const { name, email, image } = user;
-  const handleSignIn = () => {
-    const auth = getAuth();
-    signInWithPopup(auth, provider)
+  const auth = getAuth();
+  const googleHandleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
     .then((result) => {
       const { displayName, email, photoURL } = result.user;
       const loggedUser = {
@@ -27,18 +29,46 @@ function App() {
     })
   };
 
+  const gitHubHandleSignIn = () => {
+    signInWithPopup(auth, gitHubProvider)
+    .then((result) => {
+      const { displayName, email, photoURL } = result.user;
+      const loggedUser = {
+        name: displayName,
+        email,
+        image: photoURL,
+      };
+      setUser(loggedUser);
+    })
+  }
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(()=> {
+      setUser({});
+    })
+  }
+
   return (
     <div className="App">
-      <button onClick={handleSignIn}>Google Sign In</button>
-      <br />
       {
-        email && <div>
+        !name ?
+          <div>
+            <button onClick={googleHandleSignIn}>Google Sign In</button> <br /> <br />
+            <button onClick={gitHubHandleSignIn}>GitHub Sign In</button>
+          </div> :
+          <button onClick={handleSignOut}>Sign Out</button>
+      }
+      {
+        name && <div>
           <h2>Welcome {name}</h2>
           <h2>Your Email: {email}</h2>
           <h3>Your Picture:</h3>
           <img src={image} alt="" />
         </div>
       }
+      <br />
+      <br />
     </div>
   );
 }
