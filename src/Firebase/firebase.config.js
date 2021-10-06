fbffa7df5748c7187f5a5a09a48927347b3099b9
mahabubimon <mahabubemon24@gmail.com>
2021-10-07 02:05:46 +0600

    env file update

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBFFuiGu2dcMfLqLGCWH8ISHJy3ppjvMEM",
-    authDomain: "simple-firebase-authenti-6800f.firebaseapp.com",
-    projectId: "simple-firebase-authenti-6800f",
-    storageBucket: "simple-firebase-authenti-6800f.appspot.com",
-    messagingSenderId: "943210826469",
-    appId: "1:943210826469:web:450d216481326ff2b0e20c"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID
+};
 
-  export default firebaseConfig;
+export default firebaseConfig;
